7253084fbf5ac3d5d0a87af55743ade2acb73a38
Ovidiu Postoiu <postoiu.o@gmail.com>
2025-02-19 16:21:12 +0200

    Added logging and request logging plus basic middleware

--- app.js
+++ app.js
@@ -2,18 +2,29 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const morgan = require('morgan')
 const blogsRouter = require('./controlers/blog')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
 
+logger.info('connecting to', config.MONGODB_URI)
+
 mongoose.connect(config.MONGODB_URI)
-  .then(() => console.log('connected to MongoDB'))
-  .catch(err => console.log('connection to MongoDB failed:', err.message))
+  .then(() => logger.info('connected to MongoDB'))
+  .catch(err => logger.error('connection to MongoDB failed:', err.message))
+
+morgan.token('body', (request,) => JSON.stringify(request.body))
 
 app.use(cors())
 app.use(express.json())
+app.use(morgan(middleware.requestLogger))
 
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
---
+++ utils/middleware.js
@@ -0,0 +1,39 @@
+const logger = require('./logger')
+
+const requestLogger = (tokens, request, response) => {
+  const result = [
+    tokens.method(request, response),
+    tokens.url(request, response),
+    tokens.status(request, response),
+    tokens.res(request, response, 'content-length'), '-',
+    tokens['response-time'](request, response), 'ms'
+  ]
+
+  if(request.method === 'POST') {
+    result.push(tokens.body(request, response))
+  }
+
+  return result.join(' ')
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+  logger.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+module.exports = {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler
+}
